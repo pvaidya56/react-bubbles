9013550b650feb43206f7292e50cb9c50e022de0
Priya <pvaidya56@gmail.com>
2019-12-17 01:46:22 -0800

    Added access control allow origin, but still having errors logging in

--- client/src/utils/AxiosWithAuth.js
+++ client/src/utils/AxiosWithAuth.js
@@ -4,10 +4,11 @@ export const axiosWithAuth = () => {
 const token = localStorage.getItem("token");
 
 return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "http://localhost:5000",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `${token}`,
+        'Access-Control-Allow-Origin' : '*',
     },
 });
 };
